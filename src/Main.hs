313ee3712175303fe47bd1805406a04276d06ab1
BrentOnWin <brent.g.muller@gmail.com>
2020-06-28 13:19:38 -0600

    added default splices. <bind> and <apply> now work

--- src/Main.hs
+++ src/Main.hs
@@ -26,40 +26,48 @@ import Lens.Simple as L
 templateLocation::Text
 templateLocation = "c:/Users/brent/toolcrib/templates/"
 --------------------------------------------------------------------------------
+connectionString::String
 connectionString = "Driver={MySQL ODBC 8.0 Unicode Driver};\
                             \Server=192.168.0.3;Database=diam_oj;\
                             \Uid=root;Pwd=12thandEastman;"
 --------------------------------------------------------------------------------
 main :: IO ()
 main = do
-
+-- {-
     conn<-handleSqlError $ do
         conn <- connectODBC connectionString 
         return conn
     result<- quickQuery conn "Select * from tools" []
-    let hc = set hcNamespace "" emptyHeistConfig::HeistConfig ActionM
+--    print $ "result: " ++ show result
+ --   -}
+--    conn<-catchSql connectDB (\e->print e)
+    ----
     --
-    let hc2 = hcTemplateLocations .~ [loadTemplates $ T.unpack templateLocation] $ hc
 
-    let hc3 = hcInterpretedSplices .~ ("nullSplice" ## nullSplice) $ hc2
-    heistResult <-initHeist (hc3::HeistConfig ActionM)
+    let hc =  set hcInterpretedSplices defaultInterpretedSplices $
+              hcInterpretedSplices .~ ("nullSplice" ## nullSplice) $ 
+              hcTemplateLocations .~ [loadTemplates $ T.unpack templateLocation] $ 
+              set hcNamespace "" emptyHeistConfig::HeistConfig ActionM
+
+    eitherHc <- initHeist (hc::HeistConfig ActionM)
+    let hSt = getState eitherHc
     ------
     scotty 3000 $ do
       get "/test/:word" $ do
         beam <- Scty.param "word"
         Scty.html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]    
+    ----
       addroute GET "/" $ Scty.text "dammit, jim, I'm a doctor, not a haskell programmer"
       let result' = fmap (++ [SqlString"\n"]) result ::[[SqlValue]]
       let a = Prelude.concat result' :: [SqlValue]
       let b = fmap ((T.append "\t").fromSql) a :: [Text]
       let d = T.concat b :: Text
       addroute GET "/test2" $ Scty.text $ d
---
+    ----
       addroute GET "/splice" $ do -- $ Scty.html $ TE.decodeUtf8 $ toLazyByteString bdr
-        let st = getState heistResult
-        liftAndCatchIO $ print ("heist templates: " ++ (show $templateNames st))
-        liftAndCatchIO $ print ("heist splices: " ++ (show $spliceNames st))
-        maybBuilder<- renderTemplate st "test"
+        liftAndCatchIO $ print ("heist templates: " ++ (show $templateNames hSt))
+        liftAndCatchIO $ print ("heist splices: " ++ (show $spliceNames hSt))
+        maybBuilder<- renderTemplate hSt "test"
         bdr<- case maybBuilder of
             Just (buildr,mime)-> do
 --                print $ "mimeType: " ++ BS.unpack mime
@@ -77,7 +85,12 @@ getState hs= do
      case hs of
         Right r->r
         Left s -> error $ show s
-
+--------------------------------------------------------------------------------
+connectDB::IO Connection
+connectDB = do
+    conn <- connectODBC connectionString 
+    return conn
+--------------------------------------------------------------------------------
 
 
 
